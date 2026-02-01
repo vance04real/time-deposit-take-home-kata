dceeef37381c75259dc13f63a741ea4c3cb43716
evanskfchikuni <vance04real@gmail.com>
2026-02-01 08:54:29 +0200

    **[develop] Add Spring Boot main application class**
    - Created `TimeDepositKataApplication` as the main entry point for the Spring Boot application.
    - Annotated the class with `@SpringBootApplication`.
    
    Summary: Added the main application class to initialize the Spring Boot application.

---
+++ java/src/main/java/org/ikigaidigital/TimeDepositKataApplication.java
@@ -0,0 +1,12 @@
+package org.ikigaidigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDepositKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeDepositKataApplication.class, args);
+    }
+}
