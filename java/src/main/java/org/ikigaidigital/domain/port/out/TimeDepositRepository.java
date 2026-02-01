c03df0354c5ea11d013718571f854ae16480df5d
evanskfchikuni <vance04real@gmail.com>
2026-02-01 13:11:37 +0200

    **[develop] Remove unnecessary blank lines from `TimeDepositRepository`**
    - Eliminated blank lines between method declarations for better readability.
    - Ensured consistent formatting within the `TimeDepositRepository` interface.
    
    Summary: Refined `TimeDepositRepository` by removing redundant blank lines to improve code clarity and aesthetics.

--- java/src/main/java/org/ikigaidigital/domain/port/out/TimeDepositRepository.java
+++ java/src/main/java/org/ikigaidigital/domain/port/out/TimeDepositRepository.java
@@ -8,10 +8,7 @@ import java.util.List;
 
 public interface TimeDepositRepository {
     List<TimeDeposit> findAll();
-
     Page<TimeDeposit> findAll(Pageable pageable);
-
     void saveAll(List<TimeDeposit> timeDeposits);
-
     long count();
 }
